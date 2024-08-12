4c08eedf2aec7999bff972c3cc1043726b84e06b
Adam Sandham <z_a_sandham@hotmail.co.uk>
2024-08-12 21:56:04 +0000

    customized products admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import Product, Collection
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Collection)
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'collection',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductsAdmin)
+admin.site.register(Collection, CollectionAdmin)
