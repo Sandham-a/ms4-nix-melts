3173ab6d5aa8cb56b0ff1461953a305d94dccf18
Adam Sandham <z_a_sandham@hotmail.co.uk>
2024-08-20 07:18:59 +0000

    minor bug fixes

--- nix_melts/settings.py
+++ nix_melts/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vct1_p6sp&z+9&^6&k%ie(q0_6^9w9duoz60#b#2l9hc631wvz'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-sandhama-ms4nixmelts-o09uxqpr17i.ws.codeinstitute-ide.net',
